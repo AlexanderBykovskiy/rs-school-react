444ac8ad1025e4d3003056f6285a103c27c3c03f
Alexander Bykovskiy <aleksandr.bykovskiy@sksoldev.com>
2023-11-09 22:31:40 +0100

    feat: install Vitest and testing library

--- src/Components/Loader/Loader.tsx
+++ src/Components/Loader/Loader.tsx
@@ -3,8 +3,8 @@ import classes from "./Loader.styles.module.css";
 
 const Loader: React.FC = () => {
   return (
-    <div className={classes.wrapper}>
-      <div>Loading...</div>
+    <div data-testid="loader" className={classes.wrapper}>
+      <div data-testid="loader-message">Loading...</div>
     </div>
   );
 };
